aab4733c9adf50f7ce4cfd2ff8ee64be30779f11
Lino Munoz <linomu@unicauca.edu.co>
2024-08-30 15:02:14 -0500

    Se agrega input select en parametrización

--- src/app/dashboard/pages/parametrizacion/parametrizacion.component.ts
+++ src/app/dashboard/pages/parametrizacion/parametrizacion.component.ts
@@ -1,12 +1,81 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import {
+  MatSlideToggleModule,
+} from '@angular/material/slide-toggle';
+
+
+export interface TipoDocumento {
+  id: string;
+  descripcion: string;
+}
+
 
 @Component({
   selector: 'app-parametrizacion',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    FormsModule,
+    MatSlideToggleModule,
+    MatDividerModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './parametrizacion.component.html',
   styleUrl: './parametrizacion.component.css'
 })
+
 export class ParametrizacionComponent {
 
+  tiposDocumento: TipoDocumento[] = [];
+  idDocSeleccionado: string | undefined;
+  formPersona!: FormGroup;
+
+  ngOnInit() {
+    this.tiposDocumento.push({
+      id: '1',
+      descripcion: 'El Gelver'
+    });
+
+    this.tiposDocumento.push({
+      id: '2',
+      descripcion: 'El Juan Cuadrado'
+    });
+
+
+    this.tiposDocumento.push({
+      id: '3',
+      descripcion: 'El Carlos Raba'
+    });
+
+    this.tiposDocumento.push({
+      id: '4',
+      descripcion: 'El Pablo'
+    });
+
+    
+
+    this.idDocSeleccionado = this.tiposDocumento[0].id;
+    this.construirFormulario();
+  }
+
+  construirFormulario() {
+    this.formPersona = new FormGroup({
+      tipo_documento_id: new FormControl(this.idDocSeleccionado, [Validators.required]),
+    })
+  }
 }
+
+
