d7f00eca04a35c571a0429a77e22cb0e1b4d7828
Diego <diego.calvera@intempo.co>
2024-09-04 10:39:08 -0500

    se agregan campos de polizas y cargue masivo

--- src/app/model/TaxistaDTO.ts
+++ src/app/model/TaxistaDTO.ts
@@ -1,11 +1,12 @@
 export interface Taxista {
-    id: string;
-    nombre: string;
-    apellido: string;
-    celular: string;
-    cedula: string;
-    correo: string;
-    placa_taxi: string;
-    direccion: string;
-    empresa: string;
+  id: string;
+  nombre: string;
+  apellido: string;
+  celular: string;
+  cedula: string;
+  correo: string;
+  placa_taxi: string;
+  direccion: string;
+  empresa: string;
+  foto: string;
 }
